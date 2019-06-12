09db904b6b2e17e5d0582b56106a2696e62626e6
bhanna1693 <bhanna1693@gmail.com>
2019-06-11 23:07:10 -0400

    greeting component and routing created

--- src/main/webapp/java-angular-frontend/src/app/app-routing.module.ts
+++ src/main/webapp/java-angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GreetingComponent} from "./greeting/greeting.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'greeting',
+    component: GreetingComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
